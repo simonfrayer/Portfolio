3bd15084a581e8f3df32d92f4f4ee369296bbf83
simonfrayer <simon.mayer@seznam.cz>
2022-10-09 14:52:43 +0200

    about me finished setup

--- Portfolio/src/components/About Me.vue
+++ Portfolio/src/components/About Me.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="aboutme">
+    <div class="hero">
 
         <div class="heading">
             <span class="name"><b>Simon Mayer</b></span>
@@ -8,19 +8,37 @@
 
         <img src="../assets/images/profile-pic.png" alt="Profile Picture">
     </div>
+
+    <div class="aboutme" >
+        <p class="descriptionText">
+            Currently studying Software Engineering at VIA University College. I have successfully finished 2 projects (single user and client/server systems) that me and my team worked on during our school semesters. Apart from that I have put together a new team of colleges with whom we are currently designing and implementing a website for our customer - A building with apartments for rent. I am a quick learner and determined to achieve my goals. I have no problem with learning new technologies for example for my website project I had to learn to use Vue.js and Tailwind CSS on my own. I also did an on side course about Python during my highs school education. 
+        </p>
+        <div class="textBackground"></div>
+    </div>
 </template>
 
 <script>
+import descriptionText from "../assets/data/descriptiontext.json";
+
     export default {
         props:{
             
-        }
+        },
+
+        data() {
+            return {
+            }
+        },
+
+        methods: {
+            
+        },
     }
 </script>
 
 <style lang="scss" scoped>
 
-    .aboutme{
+    .hero{
         display: flex;
         flex-direction: row;
         justify-content: space-evenly;
@@ -53,6 +71,34 @@
     .heading span{
         padding: 2vw;
     }
+
+    .aboutme{
+        position: relative;
+        margin: 5vw;
+    }
+
+    .descriptionText{
+        font-size: 1rem;
+        color: #DECEC8 ;
+        z-index: 30 ;
+        padding: 5vw;
+    }
+
+    .aboutme .textBackground{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #11201E;
+        opacity: 100%;
+        z-index:-1;
+        border-radius: 5vw;
+        box-shadow: 0 0px 20px 0;
+    }
+
+
+    //media -----------------------------------------------------------
 
     @media screen and (max-width: 768px){
         .heading{font-size: 1.5rem;}
